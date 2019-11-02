f06859e2288184bdb524963de35c7b260645c0a0
HyunAm0225 <tlfgjawlq@naver.com>
2019-11-02 23:36:20 +0900

    collectstatic

---
+++ static/js/domdig/README.md
@@ -0,0 +1,62 @@
+## DOMDig
+DOMDig is a DOM XSS scanner that runs inside the Chromium web browser and it can scan single page applications (SPA) recursively.  
+Unlike other scanners, DOMDig can crawl any webapplication (including gmail) by keeping track of DOM modifications and XHR/fetch/websocket requests and it can simulate a real user interaction by firing events. During this process, XSS payloads are put into input fields and their execution is tracked in order to find injection points and the related URL modifications.  
+It is based on [htcrawl](https://htcrawl.org), a node library powerful enough to easily crawl a gmail account.
+
+
+## KEY FEATURES
+- Runs inside a real browser (Chromium)
+- Recursive DOM crawling engine
+- Handles XHR, fetch, JSONP and websockets requests
+- Supports cookies, proxy, custom headers, http auth and more
+- Scriptable login sequences
+
+## GETTING STARTED
+### Installation
+```
+git clone https://github.com/fcavallarin/domdig.git
+cd domdig && npm i && cd ..
+node domdig/domdig.js
+```
+
+### Example
+```
+node domdig.js -c 'foo=bar' -p http:127.0.0.1:8080 https://htcap.org/scanme/domxss.php
+```
+
+### Crawl Engine
+DOMDig uses [htcrawl](https://htcrawl.org) as crawling engine, the same engine used by [htcap](https://htcap.org).  
+The diagram shows the recursive crawling proccess.  
+![SPA Crawling Diagram](https://htcrawl.org/img/htcap-flowchart.png).   
+The video below shows the engine crawling gmail. The crawl lasted for many hours and about 3000 XHR request have been captured.
+
+[![crawling gmail](https://htcap.org/img/htcap-gmail-video.png)](https://www.youtube.com/watch?v=5FLmWjKE2JI "HTCAP Crawling Gmail")
+
+### Login Sequence
+A login sequence (or initial sequence) is a json object containing a list of actions to take before the scan starts.
+Each element of the list is an array where the first element is the name of the action to take and the remaining elements are "parameters" to those actions.
+Actions are:
+- write &lt;selector&gt; &lt;text&gt;
+- click &lt;selector&gt;
+- clickToNavigate &lt;selector&gt;
+- sleep &lt;seconds&gt;
+
+#### Example
+```
+[
+   ["write", "#username", "demo"],
+   ["write", "#password", "demo"],
+   ["clickToNavigate", "#btn-login"]
+]
+```
+
+### Payloads file
+Payloads can be loaded from json file (-P option) as array of strings. To build custom payloads, the string `window.___xssSink({0})` must be used as the function to be executed (instead of the classic `alert(1)`)
+
+#### Example
+```
+[
+   ';window.___xssSink({0});',
+   '<img src="a" onerror="window.___xssSink({0})">'
+]
+```
---
+++ static/js/domdig/domdig.js
@@ -0,0 +1,172 @@
+const htcrawl = require('htcrawl');
+const utils = require('./utils');
+const defpayloads = require('./payloads').all;
+const URL = require('url').URL;
+const PAYLOADMAP = {};
+const VULNSJAR = [];
+var VERBOSE = true;
+var PREVURL = null;
+
+function getNewPayload(payload, element){
+	const k = "" + Math.floor(Math.random()*4000000000);
+	const p = payload.replace("{0}", k);
+	PAYLOADMAP[k] = {payload:payload, element:element};
+	return p;
+}
+
+async function crawlAndFuzz(targetUrl, payload, options){
+	var hashSet = false;
+
+	// instantiate htcrawl
+	const crawler = await htcrawl.launch(targetUrl, options);
+
+	// set a sink on page scope
+	crawler.page().exposeFunction("___xssSink", function(key) {
+		var url = "";
+		if(crawler.page().url() != PREVURL){
+			url = PREVURL = crawler.page().url();
+		}
+		utils.addVulnerability(PAYLOADMAP[key], VULNSJAR, url, VERBOSE);
+	});
+
+	// fill all inputs with a payload
+	crawler.on("fillinput", async function(e, crawler){
+		const p = getNewPayload(payload, e.params.element);
+		try{
+			await crawler.page().$eval(e.params.element, (i, p) => i.value = p, p);
+		}catch(e){}
+		// return false to prevent element to be automatically filled with a random value
+		return false;
+	});
+
+
+	// change page hash before the triggering of the first event
+	crawler.on("triggerevent", async function(e, crawler){
+		if(!hashSet){
+			const p = getNewPayload(payload, "hash");
+			await crawler.page().evaluate(p => document.location.hash = p, p);
+			hashSet = true;
+			PREVURL = crawler.page().url();
+		}
+	});
+
+	try{
+		await crawler.load();
+	} catch(e){
+		console.log(`Error ${e}`);
+		process.exit(-3);
+	}
+
+	PREVURL = crawler.page().url();
+
+	if(options.initSequence){
+		let seqline = 1;
+		for(let seq of options.initSequence){
+			switch(seq[0]){
+				case "sleep":
+					await crawler.page().waitFor(seq[1]);
+					break;
+				case "write":
+					try{
+						await crawler.page().type(seq[1], seq[2]);
+					} catch(e){
+						utils.sequenceError("element not found", seqline);
+					}
+					break;
+				case "click":
+					try{
+						await crawler.page().click(seq[1]);
+					} catch(e){
+						utils.sequenceError("element not found", seqline);
+					}
+					await crawler.waitForRequestsCompletion();
+					break;
+				case "clickToNavigate":
+					try{
+						await crawler.clickToNavigate(seq[1], seq[2]);
+					} catch(err){
+						utils.sequenceError(err, seqline);
+					}
+					break;
+				default:
+					utils.sequenceError("action not found", seqline);
+			}
+			seqline++;
+		}
+	}
+
+	try{
+		// workaround, fix it in htcrawl
+		let timeo = setTimeout(function(){
+			crawler.stop();
+		}, options.maxExecTime);
+
+		await crawler.start();
+		clearTimeout(timeo);
+	} catch(e){
+		console.log(`Error ${e}`);
+		process.exit(-4);
+	}
+	try{
+		await crawler.reload();
+	}catch(e){
+		utils.error(e);
+	}
+	await crawler.page().waitFor(200);
+	crawler.browser().close();
+}
+
+function ps(message){
+	if(VERBOSE)utils.printStatus(message);
+}
+
+(async () => {
+	var targetUrl;
+	const argv = require('minimist')(process.argv.slice(2), {boolean:["l", "J", "q"]});
+	if(argv.q)VERBOSE = false;
+	if(VERBOSE)utils.banner();
+	if('h' in argv){
+		utils.usage();
+		process.exit(0);
+	}
+
+	if(argv._.length == 0){
+		utils.usage();
+		process.exit(1);
+	}
+
+	try{
+		targetUrl = new URL(argv._[0]);
+	} catch(e){
+		utils.error(e);
+	}
+
+	const options = utils.parseArgs(argv, targetUrl);
+
+	var payloads = argv.P ? utils.loadPayloadsFromFile(argv.P) : defpayloads;
+	ps("starting scan");
+	let cnt = 1;
+	for(let payload of payloads){
+		ps("crawling page");
+		await crawlAndFuzz(targetUrl.href, payload, options);
+		ps(cnt + "/" + payloads.length + " payloads checked");
+		cnt++;
+	}
+
+	if(VERBOSE)console.log("");
+	ps("scan finished, tot vulnerabilities: " + VULNSJAR.length);
+
+	if(argv.J){
+		console.log(utils.prettifyJson(VULNSJAR));
+	} else if(VERBOSE){
+		for(let v of VULNSJAR){
+			utils.printVulnerability(v[0], v[1], v[2]);
+		}
+	}
+
+	if(argv.o){
+		let fn = utils.writeJSON(argv.o, VULNSJAR);
+		ps("findings saved to " + fn)
+	}
+	process.exit(0);
+})();
